a66539ba7b4972ec14dd6c8fd77d3ad895b2d520
Olcne-Builder Jenkins <olcne-builder_us@oracle.com>
2025-08-26 19:08:10 +0000

    Updated to v2.16.0 from https://github.com/kubernetes/kube-state-metrics.git
    Generated from template at ecc6435ea5a15a671b9d227c8277bb46e82196ea

---
+++ olm/build-image.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+mkdir -p bin
+name="kube-state-metrics"
+version="2.16.0"
+registry="container-registry.oracle.com/olcne"
+docker_tag=${registry}/${name}:v${version}
+ldflags="
+      -s
+      -w
+      -X main.version=v%{version}
+      -X github.com/prometheus/common/version.Version=${version}
+      -X github.com/prometheus/common/version.Revision=${GIT_REVISION}
+      -X github.com/prometheus/common/version.Branch=${BRANCH}
+      -X github.com/prometheus/common/version.BuildUser=${USER}@${HOST}
+      -X github.com/prometheus/common/version.BuildDate=${BUILD_DATE}"
+
+docker build --pull \
+    --build-arg https_proxy=${https_proxy} \
+    -t ${docker_tag} -f ./olm/builds/Dockerfile .
+docker save -o ${name}.tar ${docker_tag}
