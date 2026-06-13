1fde9986535bbe93acf389d6fe18d0af6f4ac193
Oracle Cloud Native Environment Authors <noreply@oracle.com>
2026-06-13 00:58:59 +0000

    Updated to v2.19.1 from https://github.com/kubernetes/kube-state-metrics.git
    Generated from template at 106b5697f746c10f8df5792f5353905cc3aec32e

---
+++ olm/build-image.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+mkdir -p bin
+name="kube-state-metrics"
+version="2.19.1"
+registry="container-registry.oracle.com/olcne"
+docker_tag=${registry}/${name}:v${version}
+
+patch < olm/Makefile.patch
+
+docker build --pull \
+    --build-arg https_proxy=${https_proxy} \
+    -t ${docker_tag} -f ./olm/builds/Dockerfile .
+docker save -o ${name}.tar ${docker_tag}
